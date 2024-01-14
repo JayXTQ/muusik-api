3783f9c5454839969a67f294e20991785d8d3bd2
rad <me@radnotred.dev>
2024-01-14 18:23:26 -0500

    feat: auto adding help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,26 +1,25 @@
+const fs = require('fs');
+const path = require('path');
 import { CommandInteraction } from 'discord.js';
+import { Command } from '../types';
 
 export const helpCommand = async (interaction: CommandInteraction) => {
     if (interaction.commandName === "help") {
+        const commandsFilePath = path.join(__dirname, '../../commands.json');
+        const commands: Command[] = JSON.parse(fs.readFileSync(commandsFilePath, 'utf8'));
+
+        const fields = commands.map((cmd: Command) => ({
+            name: `/${cmd.name}`,
+            value: cmd.description
+        }));
+
         interaction.reply({
             "embeds": [
                 {
-                    "title": `muusik.app`,
-                    "description":
-                        `Hello! I am the muusik.app bot written by [Jay](https://jayxtq.xyz). Thank you for using muusik.app! If you want to play music please use the [muusik.app website](https://muusik.app). For commands, check below!`,
+                    "title": `muusik.app Commands`,
+                    "description": `Here are the commands you can use with the muusik.app bot:`,
                     "color": 0x3A015C,
-                    "fields": [
-                        {
-                            "name": `/info`,
-                            "value":
-                                `Get information regarding the bot`,
-                        },
-                        {
-                            "name": `/help`,
-                            "value":
-                                `Get help with the muusik.app bot`,
-                        }
-                    ],
+                    "fields": fields,
                 },
             ],
         });
