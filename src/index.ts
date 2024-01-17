1a3eadabd8a33e56bc6b2801841698b0ba489275
Jay <me@jayxtq.xyz>
2024-01-17 20:52:17 +0000

    fix: a radical mistake :unamused:

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { ActivityType, Client, GatewayIntentBits, REST } from "discord.js";
-import { ActivityType, Client, GatewayIntentBits, REST } from "discord.js";
 import { Player } from 'discord-player';
 import * as routeHandlers from './routes/index';
 import { interactionManager } from './modules/interactionManager';
